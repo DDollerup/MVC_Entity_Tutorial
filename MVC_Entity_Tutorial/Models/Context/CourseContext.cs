47f828a17390d791625f2446c63c88b026ebd45f
PDDaniel <daniel@proper-dog.com>
2016-06-27 15:00:57 +0200

    Save

--- MVC_Entity_Tutorial/Models/Context/CourseContext.cs
+++ MVC_Entity_Tutorial/Models/Context/CourseContext.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using MVC_Entity_Tutorial.Models;
 
 namespace MVC_Entity_Tutorial.Models.Context
 {
     public class CourseContext : DbContext
     {
-        public CourseContext() : base("ConnectionString")
-        {
+        public CourseContext() : base("csCourses") { }
 
-        }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
     }
 }
